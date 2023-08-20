58644d166ee9453f65e246e36b5d834913f01ddd
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-08-20 15:50:43 -0500

    updated not found page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <>
+    <div className="not-found-section">
       <h1>404 - Page Not Found</h1>
       <p>Look elsewhere please. Thank you.</p>
-    </>
+    </div>
   );
 };
 
