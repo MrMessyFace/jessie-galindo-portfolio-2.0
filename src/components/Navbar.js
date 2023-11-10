964739e7cc10bd77cb962428c9601b05d38d05df
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-11-10 13:20:34 -0600

    updated resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import pdf from "../assets/pdfs/jessiesgalindo-resume.pdf";
+import pdf from "../assets/pdfs/jessie-galindo-resume.pdf";
 
 const Navbar = () => {
   return (
